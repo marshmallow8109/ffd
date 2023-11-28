104ec71f88d681ce0c3a2789b0cc71046cb0da92
Austine <augustrush.ae@gmail.com>
2023-11-28 14:11:35 +0100

    updated js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 console.log("hello code");
 
 const name = "August";
-const data = 23;
+const data = "45";
 
 const getName = (name, data) => {
   return name + data;
 };
 
 console.log(getName(name, data));
+console.log("more codes here");
